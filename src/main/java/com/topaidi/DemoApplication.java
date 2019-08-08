b4bb8edfa77241c9478e5665d800b844ce6a76b9
ThomasLhomme <lhomme.thomas@gmail.com>
2019-08-08 14:53:55 +0200

    Adding support for tomcat

--- src/main/java/com/topaidi/DemoApplication.java
+++ src/main/java/com/topaidi/DemoApplication.java
@@ -2,12 +2,17 @@ package com.topaidi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends ServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DemoApplication.class);
+	}
 }
